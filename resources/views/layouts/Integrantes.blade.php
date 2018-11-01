91e77f5513992c4bf26068496958a7eaa31cdf33
BlackSpider232 <jeredick.escobar96@gmail.com>
2018-10-31 19:50:45 -0600

    Pantalla Integrantes
    Si tengo tiempo le hago mas cambios

---
+++ resources/views/layouts/Integrantes.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Integrantes de Grupo</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/resume.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="blog">
+        <span class="d-block d-lg-none">Clarence Taylor</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="Integrantes_muestra">
+        <div class="my-auto">
+          <h2 class="mb-0">Inmer Omar
+            <span class="text-primary">Balcarcel Garrido</span>
+            <h3 class="mb-0">
+                <span class="text-primary">0900-10-2518</span>
+            </h3>
+          </h2>
+
+          <h2 class="mb-0">Kelvin
+            <span class="text-primary">André</span>
+            Sarat
+            <span class="text-primary">Gramajo</span>
+            <h3 class="mb-0">
+                <span class="text-primary">0900-14-1000</span>
+            </h3>
+          </h2>
+
+          <h2 class="mb-0">
+            <span class="text-primary">Jeredick Alessandro</span>
+            Escobar Monroy
+            <h3 class="mb-0">
+                <span class="text-primary">0900-10-2518</span>
+            </h3>
+          </h2>
+          <h2 class="mb-0">Adriana Estefanía
+                <span class="text-primary">Ruíz Baechli </span>
+                <h3 class="mb-0">
+                    <span class="text-primary">0901-14-315</span>
+                </h3>
+          </h2>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/resume.min.js"></script>
+
+  </body>
+
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,10 +45,11 @@
                         @guest
                             <li><a href="{{ route('login') }}">Iniciar sesion</a></li>
                             <li><a href="{{ route('register') }}">Registrarse</a></li>
+                            <li><a href="integrantes">Integrantes del grupo</a></li>
                         @else
                             <li><a href="{{ route('tags.index') }}">Etiquetas</a></li>
                             <li><a href="{{ route('categories.index') }}">Categorías</a></li>
-                            <li><a href="{{ route('posts.index') }}">Entradas</a></li>                            
+                            <li><a href="{{ route('posts.index') }}">Entradas</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -86,7 +87,7 @@
             </div>
         @endif
 
-        @if(count($errors))            
+        @if(count($errors))
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
@@ -103,7 +104,7 @@
         @endif
 
         @yield('content')
-        
+
     </div>
 
     <!-- Scripts -->
